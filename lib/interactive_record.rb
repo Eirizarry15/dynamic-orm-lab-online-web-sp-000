270fd4dbe145a1d4111e37eb6c08aa485f7bdaaf
Edgar Irizarry <eirizarry15@yahoo.com>
2019-02-15 16:12:49 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -35,6 +35,28 @@ def initialize(options={})
     self.class.table_name
   end
 
+def values_for_insert
+    values = []
+    self.class.column_names.each do |col_name|
+      values << "'#{send(col_name)}'" unless send(col_name).nil?
+    end
+    values.join(", ")
+  end
 
+  def col_names_for_insert
+    self.class.column_names.delete_if {|col| col == "id"}.join(", ")
+  end
+
+  def self.find_by_name(name)
+    sql = "SELECT * FROM #{self.table_name} WHERE name = '#{name}'"
+    DB[:conn].execute(sql)
+  end
+
+  def self.find_by(attr_hash)
+    column=attr_hash.keys[0].to_s
+    value = attr_hash[attr_hash.keys[0]]
+    sql="SELECT * FROM #{self.table_name} WHERE #{column} = '#{value}'"
+    DB[:conn].execute(sql)
+  endß
 
 end
